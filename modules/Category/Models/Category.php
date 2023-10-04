f335c76806f0976bb5c89af3e2a5e4e8680300cd
leila <leilanouri22@yahoo.com>
2023-10-04 11:12:48 +0330

    add CategoryController,Models, Migration ,... for Category module

---
+++ modules/Category/Models/Category.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Category\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    protected $guarded=['id'];
+    public function parentCategory(){
+        return $this->belongsTo(Category::class,'parent_id');
+    }
+    public function subCategories(){
+        return $this->hasMany(Category::class,'parent_id');
+    }
+    public function categoryOne(){
+        return $this->hasOne(Category::class,'parent_id');
+    }
+    public function subcategoryOne(){
+        return $this->belongsTo(Category::class,'parent_id');
+    }
+}
