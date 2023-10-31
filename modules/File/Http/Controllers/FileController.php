71f8b4791405c6120f00f5863366f3d4caf2da7f
FAKHARI1 <hosein.fakhari1998@gmail.com>
2023-10-31 09:17:44 +0330

    fix in README.md

--- modules/File/Http/Controllers/FileController.php
+++ modules/File/Http/Controllers/FileController.php
@@ -3,36 +3,77 @@
 namespace File\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use File\Models\File;
+use File\Services\uploader\Uploader;
+use Illuminate\Http\Request;
+
 
 class FileController extends Controller
 {
+    private Uploader $uploader;
+
+    public function __construct(Uploader $uploader)
+    {
+      $this->uploader=$uploader;
+    }
+
     public function index()
     {
-
+        $files=File::all();
+      return view('Files::files.index',compact('files'));
     }
+
     public function create()
     {
-
+        return view('Files::files.create');
     }
+
+    public function new(Request $request)
+    {
+        try {
+            $this->validateFile($request);
+            $this->uploader->upload();
+            return redirect()->back()->withSuccess('file has uploaded successfully');
+        }catch (\Exception $e){
+            return redirect()->back()->withErrors($e->getMessage());
+
+        }
+       }
+
+    private function validateFile($request)
+    {
+        $request->validate([
+            'file'=>['required','file','mimetypes:image/jpeg,video/mp4,application/zip']
+        ]);
+    }
+
     public function store()
     {
 
     }
-    public function show($id)
+
+    public function show(File $file)
     {
+        return $file->download();
 
     }
+
     public function edit($id)
     {
 
     }
+
     public function update($id)
     {
 
     }
-    public function destroy($id)
-    {
 
+    public function delete(File $file)
+    {
+        $file->delete();
+        return back();
     }
+
+
 
 }
