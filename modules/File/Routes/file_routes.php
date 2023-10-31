4f5bfd21552f4b10374fdfcbcd273c16f7d40ca7
Hossein Fakhari <hossein.fakhari1977@gmail.com>
2023-10-31 10:30:51 +0330

    fix in file module

--- modules/File/Routes/file_routes.php
+++ modules/File/Routes/file_routes.php
@@ -1,15 +1,17 @@
 <?php
+
+use File\Http\Controllers\FileController;
 use Illuminate\Support\Facades\Route;
 
-Route::group([
-    'namespace'=>'File\Http\Controllers',
-    'middleware'=>'web'],function (){
-    Route::get('/files','FileController@index')->name('files.index');
-    Route::get('/files/create','FileController@create')->name('files.create');
-    Route::post('/files','FileController@store')->name('files.store');
-    Route::get('/files/{id}','FileController@show')->name('files.show');
-    Route::get('/files/{id}/edit','FileController@show')->name('files.edit');
-    Route::put('/files/{id}','FileController@update')->name('files.update');
-    Route::delete('/files/{id}','FileController@destroy')->name('files.destroy');
+Route::middleware('web')->group(
+   function (){
+    Route::get('file',[FileController::class,'index'])->name('file.index');
+    Route::get('file/create',[FileController::class,'create'])->name('file.create');
+    Route::get('file',[FileController::class,'new'])->name('file.new');
+    Route::post('file',[FileController::class,'store'])->name('file.store');
+    Route::get('file/{file}',[FileController::class,'show'])->name('file.show');
+    Route::get('file/{id}/edit',[FileController::class,'edit'])->name('file.edit');
+    Route::put('file/{id}',[FileController::class,'update'])->name('file.update');
+    Route::get('file/delete/{file}',[FileController::class,'delete'])->name('file.delete');
 
 });
