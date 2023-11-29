e2c259e143b2da90ee890d57737ce70972114905
Hossein Fakhari <hossein.fakhari1977@gmail.com>
2023-11-29 18:25:46 +0330

    fix in scheduling module

--- modules/User/Database/Seeders/UserSeeder.php
+++ modules/User/Database/Seeders/UserSeeder.php
@@ -15,117 +15,117 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $users = [
-            [
-                'first_name' => 'مدیر',
-                'last_name' => 'کل',
-                'mobile' => '9010335657',
-                'mobile_verified_at' => null,
-                'national_code' => null,
-                'job' => null,
-                'created_at' => '2022-04-22 17:48:28',
-                'updated_at' => '2022-04-24 15:38:51',
-                'role' => Role::ROLE_SUPER_ADMIN
-            ],
-            [
-                'first_name' => 'بیمار 1',
-                'last_name' => null,
-                'mobile' => '9112367891',
-                'mobile_verified_at' => null,
-                'national_code' => null,
-                'job' => null,
-                'created_at' => '2022-04-22 17:48:28',
-                'updated_at' => '2022-04-24 15:38:51',
-                'role' => Role::ROLE_PATIENT
-            ],
-            /*[
-                'first_name' => 'patient 2',
-                'last_name' => null,
-                'mobile' => null,
-                'mobile_verified_at' => null,
-                'national_code' => null,
-                'job' => null,
-                'created_at' => '2022-04-22 17:48:28',
-                'updated_at' => '2022-04-24 15:38:51',
-                'role' => Role::ROLE_PATIENT
-            ],
-            [
-                'first_name' => 'patient 3',
-                'last_name' => null,
-                'mobile' => null,
-                'mobile_verified_at' => null,
-                'national_code' => null,
-                'job' => null,
-                'created_at' => '2022-04-22 17:48:28',
-                'updated_at' => '2022-04-24 15:38:51',
-                'role' => Role::ROLE_PATIENT
-            ],
-            [
-                'first_name' => 'patient 4',
-                'last_name' => null,
-                'mobile' => null,
-                'mobile_verified_at' => null,
-                'national_code' => null,
-                'job' => null,
-                'created_at' => '2022-04-22 17:48:28',
-                'updated_at' => '2022-04-24 15:38:51',
-                'role' => Role::ROLE_PATIENT
-            ],
-            [
-                'first_name' => 'patient 5',
-                'last_name' => null,
-                'mobile' => null,
-                'mobile_verified_at' => null,
-                'national_code' => null,
-                'job' => null,
-                'created_at' => '2022-04-22 17:48:28',
-                'updated_at' => '2022-04-24 15:38:51',
-                'role' => Role::ROLE_PATIENT
-            ],
-            [
-                'first_name' => 'patient 6',
-                'last_name' => null,
-                'mobile' => null,
-                'mobile_verified_at' => null,
-                'national_code' => null,
-                'job' => null,
-                'created_at' => '2022-04-22 17:48:28',
-                'updated_at' => '2022-04-24 15:38:51',
-                'role' => Role::ROLE_PATIENT
-            ],
-            [
-                'first_name' => 'patient 7',
-                'last_name' => null,
-                'mobile' => null,
-                'mobile_verified_at' => null,
-                'national_code' => null,
-                'job' => null,
-                'created_at' => '2022-04-22 17:48:28',
-                'updated_at' => '2022-04-24 15:38:51',
-                'role' => Role::ROLE_PATIENT
-            ],
-            [
-                'first_name' => 'patient 8',
-                'last_name' => null,
-                'mobile' => null,
-                'mobile_verified_at' => null,
-                'national_code' => null,
-                'job' => null,
-                'created_at' => '2022-04-22 17:48:28',
-                'updated_at' => '2022-04-24 15:38:51',
-                'role' => Role::ROLE_PATIENT
-            ],*/
-
-        ];
-
-        foreach ($users as $user) {
-            User::create([
-                'mobile' => $user['mobile'],
-                'first_name' => $user['first_name'],
-                'last_name' => $user['last_name'],
-                'created_at' => $user['created_at'],
-                'updated_at' => $user['updated_at']
-            ])->assignRole($user['role']);
-        }
+//        $users = [
+//            [
+//                'first_name' => 'مدیر',
+//                'last_name' => 'کل',
+//                'mobile' => '9010335657',
+//                'mobile_verified_at' => null,
+//                'national_code' => null,
+//                'job' => null,
+//                'created_at' => '2022-04-22 17:48:28',
+//                'updated_at' => '2022-04-24 15:38:51',
+//                'role' => Role::ROLE_SUPER_ADMIN
+//            ],
+//            [
+//                'first_name' => 'بیمار 1',
+//                'last_name' => null,
+//                'mobile' => '9112367891',
+//                'mobile_verified_at' => null,
+//                'national_code' => null,
+//                'job' => null,
+//                'created_at' => '2022-04-22 17:48:28',
+//                'updated_at' => '2022-04-24 15:38:51',
+//                'role' => Role::ROLE_PATIENT
+//            ],
+//            /*[
+//                'first_name' => 'patient 2',
+//                'last_name' => null,
+//                'mobile' => null,
+//                'mobile_verified_at' => null,
+//                'national_code' => null,
+//                'job' => null,
+//                'created_at' => '2022-04-22 17:48:28',
+//                'updated_at' => '2022-04-24 15:38:51',
+//                'role' => Role::ROLE_PATIENT
+//            ],
+//            [
+//                'first_name' => 'patient 3',
+//                'last_name' => null,
+//                'mobile' => null,
+//                'mobile_verified_at' => null,
+//                'national_code' => null,
+//                'job' => null,
+//                'created_at' => '2022-04-22 17:48:28',
+//                'updated_at' => '2022-04-24 15:38:51',
+//                'role' => Role::ROLE_PATIENT
+//            ],
+//            [
+//                'first_name' => 'patient 4',
+//                'last_name' => null,
+//                'mobile' => null,
+//                'mobile_verified_at' => null,
+//                'national_code' => null,
+//                'job' => null,
+//                'created_at' => '2022-04-22 17:48:28',
+//                'updated_at' => '2022-04-24 15:38:51',
+//                'role' => Role::ROLE_PATIENT
+//            ],
+//            [
+//                'first_name' => 'patient 5',
+//                'last_name' => null,
+//                'mobile' => null,
+//                'mobile_verified_at' => null,
+//                'national_code' => null,
+//                'job' => null,
+//                'created_at' => '2022-04-22 17:48:28',
+//                'updated_at' => '2022-04-24 15:38:51',
+//                'role' => Role::ROLE_PATIENT
+//            ],
+//            [
+//                'first_name' => 'patient 6',
+//                'last_name' => null,
+//                'mobile' => null,
+//                'mobile_verified_at' => null,
+//                'national_code' => null,
+//                'job' => null,
+//                'created_at' => '2022-04-22 17:48:28',
+//                'updated_at' => '2022-04-24 15:38:51',
+//                'role' => Role::ROLE_PATIENT
+//            ],
+//            [
+//                'first_name' => 'patient 7',
+//                'last_name' => null,
+//                'mobile' => null,
+//                'mobile_verified_at' => null,
+//                'national_code' => null,
+//                'job' => null,
+//                'created_at' => '2022-04-22 17:48:28',
+//                'updated_at' => '2022-04-24 15:38:51',
+//                'role' => Role::ROLE_PATIENT
+//            ],
+//            [
+//                'first_name' => 'patient 8',
+//                'last_name' => null,
+//                'mobile' => null,
+//                'mobile_verified_at' => null,
+//                'national_code' => null,
+//                'job' => null,
+//                'created_at' => '2022-04-22 17:48:28',
+//                'updated_at' => '2022-04-24 15:38:51',
+//                'role' => Role::ROLE_PATIENT
+//            ],*/
+//
+//        ];
+//
+//        foreach ($users as $user) {
+//            User::create([
+//                'mobile' => $user['mobile'],
+//                'first_name' => $user['first_name'],
+//                'last_name' => $user['last_name'],
+//                'created_at' => $user['created_at'],
+//                'updated_at' => $user['updated_at']
+//            ])->assignRole($user['role']);
+//        }
     }
 }
